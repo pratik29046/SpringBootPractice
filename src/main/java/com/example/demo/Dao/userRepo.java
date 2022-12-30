7b101e775ff9dea62d2e7d74d1e96f0270ef60d9
pratik29046 <pratikpawade8667@gmail.com>
2022-12-30 15:59:29 +0530

    find by user using name query

--- src/main/java/com/example/demo/Dao/userRepo.java
+++ src/main/java/com/example/demo/Dao/userRepo.java
@@ -3,7 +3,11 @@ package com.example.demo.Dao;
 import com.example.demo.POJO.TableUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface userRepo extends JpaRepository<TableUser,Long> {
+
+    public List<TableUser> findByName(String name);
 
 
 }
